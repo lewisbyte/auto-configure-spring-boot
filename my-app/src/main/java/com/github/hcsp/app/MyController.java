1c175d6ee66888b507fd58d99378c98537275237
lewis <lingyidx@gmail.com>
2021-02-28 22:59:31 +0800

    revert MyController.java

--- my-app/src/main/java/com/github/hcsp/app/MyController.java
+++ my-app/src/main/java/com/github/hcsp/app/MyController.java
@@ -3,16 +3,15 @@ package com.github.hcsp.app;
 import com.github.hcsp.starter.MyResponseBody;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class MyController {
-    @GetMapping("users")
+    @GetMapping("/users")
     @MyResponseBody
-    public List<User> user() {
+    public List<User> users() {
         return Arrays.asList(new User(1, "A"), new User(2, "B"));
     }
 
